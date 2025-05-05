e93718a0406509a0cb0ee8ac4f5511f77cc63744
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2025-05-05 16:42:34 +0700

    hero section done

---
+++ resources/views/components/beranda/hero-section.blade.php
@@ -0,0 +1,30 @@
+<section class="container mt-4 mx-auto p-6 max-h-screen flex gap-32 justify-center items-center">
+    <div>
+        {{-- hero title --}}
+        <div class="uppercase text-5xl font-bold leading-22">
+            <div>Menjadi <span class="bg-black text-primary">Pribadi</span></div>
+            <div>Yang <span class="bg-black text-primary">Mampu</span></div>
+            <div><span class="bg-black text-primary">Bersaing</span></div>
+            <div>Di Bidang <span class="bg-black text-primary">IT</span></div>
+        </div>
+        {{-- hero description --}}
+        <div class="leading-tight uppercase text-lightGray mb-6">
+            <p>Menjadi pribadi yang mampu berkomunikasi serta</p>
+            <p>berkolaborasi dengan baik dalam suatu projek </p>
+            <p>pengembangan aplikasi</p>
+
+        </div>
+        {{-- hero link --}}
+        <a class="text-lg font-bold flex gap-2 items-center" href="layanan">
+            <div class="py-2 px-4 border rounded-full">JELAJAHI</div>
+            <div class="p-2 rounded-full bg-black text-white w-fit">
+                <x-heroicon-s-arrow-up class="w-6 h-6 rotate-[30deg]" />
+            </div>
+        </a>
+    </div>
+    {{-- hero image --}}
+    <div class="relative">
+        <img src="/img/hero-image.png" class="w-[280px] block img-shadow" alt="">
+        <img src="/img/logo-komunitas-transparent.png" class="absolute -right-20 top-10 bottom-0 -z-10 opacity-60" alt="">
+    </div>
+</section>
