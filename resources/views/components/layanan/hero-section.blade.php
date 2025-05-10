186978b12c3eb6d6cd3f934ffb4e36494e839977
Yoga rmdn <teknooga@gmail.com>
2025-05-10 15:19:29 +0700

    Tampilan layanan 100% sesuai dengan design

--- resources/views/components/layanan/hero-section.blade.php
+++ resources/views/components/layanan/hero-section.blade.php
@@ -1,22 +1,22 @@
 <section class="font-Hammersmith-One">
-    <div class="flex gap-32 justify-between items-center">
+    <div class="flex gap-32 justify-between items-center dark:text-cyan-400">
         <div class="ml-35">
             {{-- hero title --}}
             <div class="uppercase text-7xl font-bold leading-22">
-                <span class="bg-black text-primary">wujudkan</span>
-                <div>website <span class="bg-black text-primary">anda</span></div>
-                <div>bersama <span class="bg-black text-primary">decade</span></div>
+                <span class="bg-black text-primary dark:text-black dark:bg-white">wujudkan</span>
+                <div>website <span class="bg-black text-primary dark:text-black dark:bg-white">anda</span></div>
+                <div>bersama <span class="bg-black text-primary dark:text-black dark:bg-white">decade</span></div>
             </div>
             {{-- hero description --}}
-            <div class="leading-tight uppercase text-lightGray mb-6">
+            <div class="leading-tight uppercase text-lightGray mb-6 mt-4">
                 <p>Tuangkan semua ide anda kepada kami dan kami</p>
                 <p>akan mewujudkan semuanya</p>
 
             </div>
             {{-- hero link --}}
             <a class="text-lg font-bold flex gap-2 items-center" href="layanan">
-                <div class="py-2 px-4 border rounded-full">JELAJAHI</div>
-                <div class="p-2 rounded-full bg-black text-white w-fit">
+                <div class="py-2 px-4 border rounded-full dark:bg-white dark:text-black">JELAJAHI</div>
+                <div class="p-2 rounded-full bg-black text-white dark:bg-blue-500 dark:text-white w-fit">
                     <x-heroicon-s-arrow-up class="w-6 h-6 rotate-[30deg]" />
                 </div>
             </a>
--- resources/views/components/layanan/logo-section.blade.php
+++ resources/views/components/layanan/logo-section.blade.php
@@ -1,8 +1,8 @@
 <section class="mt-10 mb-10">
-    <div class="uppercase text-6xl font-bold leading-25 text-center">
-        <div>ingin <span class="bg-black text-primary">membuat logo</span> yang</div>
-        <div>menggambarkan <span class="bg-black text-primary">kualitas</span></div>
-        <div>kami <span class="bg-black text-primary">buatkan</span></div>
+    <div class="uppercase text-6xl font-bold leading-25 text-center dark:text-cyan-400">
+        <div>ingin <span class="bg-black text-primary dark:text-black dark:bg-white">membuat logo</span> yang</div>
+        <div>menggambarkan <span class="bg-black text-primary dark:text-black dark:bg-white">kualitas</span></div>
+        <div>kami <span class="bg-black text-primary dark:text-black dark:bg-white">buatkan</span></div>
     </div>
     <div class="flex justify-center items-center gap-70 mt-4">
         <div class="leading-tight uppercase text-lightGray mb-6">
@@ -11,15 +11,16 @@
 
         </div>
         <a class="text-lg font-bold flex gap-2 items-center" href="layanan">
-            <div class="py-2 px-4 border rounded-full">JELAJAHI</div>
-            <div class="p-2 rounded-full bg-black text-white w-fit">
+            <div class="py-2 px-4 border rounded-full dark:bg-white dark:text-black">JELAJAHI</div>
+            <div class="p-2 rounded-full bg-black text-white dark:bg-blue-500 dark:text-white w-fit">
                 <x-heroicon-s-arrow-up class="w-6 h-6 rotate-[30deg]" />
             </div>
         </a>
     </div>
     <div class="flex justify-between items-center">
-        <img src="img/layanan/image8.png" alt="tumbler" class="h-auto max-w-full">
-        <img src="img/layanan/kedua.png" alt="tumbler" class="h-auto max-w-full">
-        <img src="img/layanan/image9.png" alt="tumbler" class="h-auto max-w-full">
+        <img src="img/layanan/image8.png" alt="foto_kiri" class="h-auto max-w-full">
+        <img src="img/layanan/logo-black.png" alt="foto_tengah" class="h-auto max-w-full block dark:hidden">
+        <img src="img/layanan/logo-blue.png" alt="foto_tengah" class="h-auto max-w-full hidden dark:block">
+        <img src="img/layanan/image9.png" alt="foto_kanan" class="h-auto max-w-full">
     </div>
 </section>
